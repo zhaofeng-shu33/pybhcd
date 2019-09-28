eb85520190e3cb421796550a4e7b119604522a92
zhaofeng-shu33 <616545598@qq.com>
2019-09-28 13:49:58 +0800

    add docker build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e -x
+PLAT=manylinux2010_x86_64
+PY36=/opt/python/cp36-cp36m/bin
+PY37=/opt/python/cp37-cp37m/bin
+$PY36/pip wheel . -w dist/
+$PY37/pip wheel . -w dist/
+# Bundle external shared libraries into the wheels
+for whl in dist/*.whl; do
+    auditwheel repair "$whl" --plat $PLAT -w dist/wheelhouse/
+done
