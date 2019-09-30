c14717104d9f6f9812bec2543acc82a55678376c
zhaofeng-shu33 <616545598@qq.com>
2019-09-30 11:37:15 +0800

    prepare to rewrite the bridge.

--- pybhcd.pyx
+++ pybhcd.pyx
@@ -24,7 +24,7 @@ cdef extern from "bhcd/bhcd/bhcd.h":
     GRand* g_rand_new()
     void tree_io_save_string(Tree*, gchar**)
     Build * build_new(GRand *rng, Params * params, guint num_restarts, gboolean sparse)
-    Dataset * dataset_gml_load(const gchar*)
+    Dataset* dataset_new()
     Params * params_new(Dataset *, gdouble, gdouble, gdouble, gdouble, gdouble)
     void build_set_verbose(Build * build, gboolean value)
     void params_unref(Params * params)
@@ -35,20 +35,19 @@ cdef extern from "bhcd/bhcd/bhcd.h":
     void tree_unref(Tree * tree)
     void g_rand_free(GRand* rand_)
     void g_free(gpointer mem)
-    void tree_io_save_string(Tree* tree, gchar** strbufferptr)
     
-cpdef bhcd(gml_py_str, gamma=0.4, alpha=1.0, beta=0.2, delta=1.0, _lambda=0.2, binary_only=False, restarts=1, sparse=False):
-    cdef char* gml_c_str
+cpdef bhcd(nx_obj, gamma=0.4, alpha=1.0, beta=0.2, delta=1.0, _lambda=0.2, binary_only=False, restarts=1, sparse=False):
     cdef GRand* rng_ptr
     cdef Params* params_ptr
     cdef Dataset* dataset_ptr
     cdef Build* build_ptr
     cdef Tree* root_ptr
-    cdef gchar* strbuffer
-    gml_py_byte = (gml_py_str + '\n').encode('ascii')
-    gml_c_str = gml_py_byte
+    cdef int nedges, nvertices
+    nedges = len(networkx_graph.edges)
+    nvertices = len(networkx_graph.nodes)    
+    # load dataset
+    dataset_ptr = dataset_new()
     rng_ptr = g_rand_new()
-    dataset_ptr = dataset_gml_load( <gchar*> gml_c_str)
     params_ptr = params_new(dataset_ptr, <gdouble> gamma, <gdouble> alpha, 
         <gdouble> beta, <gdouble> delta, <gdouble> _lambda)    
     params_ptr.binary_only = <gboolean> binary_only
@@ -59,10 +58,5 @@ cpdef bhcd(gml_py_str, gamma=0.4, alpha=1.0, beta=0.2, delta=1.0, _lambda=0.2, b
     root_ptr = build_get_best_tree(build_ptr)
     tree_ref(root_ptr)
     build_free(build_ptr)
-    tree_io_save_string(root_ptr, address(strbuffer))
-    cdef gint str_size = strlen(strbuffer)
     tree_unref(root_ptr)
     g_rand_free(rng_ptr)
-    cdef bytes py_result_string = strbuffer
-    g_free(strbuffer)
-    return py_result_string.decode('ascii')
