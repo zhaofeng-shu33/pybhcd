f5a9423b59aba5059227548cbc815242125eed99
zhaofeng-shu33 <616545598@qq.com>
2019-10-21 09:47:32 +0800

    predict support unobserved value

--- pybhcd.pyx
+++ pybhcd.pyx
@@ -3,6 +3,7 @@
 from libc.string cimport strlen
 from cython.operator import address
 
+import networkx as nx
 
 cdef extern from "glib.h":
     ctypedef char gchar
@@ -83,49 +84,16 @@ cdef extern from "bhcd/bhcd/bhcd.h":
     gboolean dataset_label_pairs_iter_next(DatasetPairIter*, gpointer*, gpointer*)
     gdouble tree_logpredict(Tree*, gconstpointer, gconstpointer, gboolean)
     gboolean dataset_get(Dataset*, gconstpointer, gconstpointer, gboolean*)
-    
-cdef get_edge_prediction(Dataset* dataset_ptr, Tree* tree_root_ptr):
-    cdef DatasetPairIter pairs
-    cdef gpointer src = NULL
-    cdef gpointer dst = NULL
-    cdef gboolean value, missing
-    cdef gdouble logpred_true, logpred_false
-    dataset_label_pairs_iter_init(dataset_ptr, &pairs)
-    edges = []
-    while dataset_label_pairs_iter_next(&pairs, &src, &dst):
-        value = dataset_get(dataset_ptr, src, dst, &missing)
-        logpred_true = tree_logpredict(tree_root_ptr, src, dst, 1)
-        logpred_false = tree_logpredict(tree_root_ptr, src, dst, 0)
-        py_byte_str_1 = dataset_label_to_string(dataset_ptr, src)
-        py_byte_str_2 = dataset_label_to_string(dataset_ptr, dst)
-        py_boolean = value > 0
-        edges.append([py_byte_str_1.decode('ascii'),
-                      py_byte_str_2.decode('ascii'),
-                      py_boolean,
-                      logpred_true,
-                      logpred_false])
-    return edges
+    void dataset_unref(Dataset*)
 
-cpdef bhcd(nx_obj, gamma=0.4, alpha=1.0, beta=0.2, delta=1.0, _lambda=0.2, binary_only=False, restarts=1, sparse=False):
-    cdef GRand* rng_ptr
-    cdef Params* params_ptr
-    cdef Dataset* dataset_ptr
-    cdef Build* build_ptr
-    cdef Tree* tree_root_ptr
-    cdef int nedges, nvertices
-    cdef char* node_label_c_str
-    cdef gchar* id
+cdef void construct_dataset_from_networkx_graph(nx_obj, Dataset* dataset_ptr):
     cdef GHashTable * id_labels
+    cdef char* node_label_c_str
+    cdef gpointer label
+    cdef gchar* id    
     cdef gpointer src
     cdef gpointer dst
-    cdef gpointer label
-    cdef gint next_index = -1
-    cdef Pair* cur
-    cdef Tree* tree_tmp_ptr
-    cdef gint parent_index
-    cdef GQueue * qq
-    cdef GList * child
-
+    
     nedges = len(nx_obj.edges)
     nvertices = len(nx_obj.nodes)
     # load dataset
@@ -147,6 +115,51 @@ cpdef bhcd(nx_obj, gamma=0.4, alpha=1.0, beta=0.2, delta=1.0, _lambda=0.2, binar
         dataset_set(dataset_ptr, src, dst, 1)
     g_hash_table_unref(id_labels)
     # end load dataset
+    
+cdef get_edge_prediction(num_nodes, Tree* tree_root_ptr):
+    cdef Dataset* dataset_ptr = NULL
+    cdef DatasetPairIter pairs
+    cdef gpointer src = NULL
+    cdef gpointer dst = NULL
+    cdef gboolean value, missing
+    cdef gdouble logpred_true, logpred_false
+    nx_obj = nx.Graph()
+    for i in range(num_nodes):
+        for j in range(i+1, num_nodes):
+            nx_obj.add_edge(i, j)
+    construct_dataset_from_networkx_graph(nx_obj, dataset_ptr)
+    dataset_label_pairs_iter_init(dataset_ptr, &pairs)
+    edges = []
+    while dataset_label_pairs_iter_next(&pairs, &src, &dst):
+        value = dataset_get(dataset_ptr, src, dst, &missing)
+        logpred_true = tree_logpredict(tree_root_ptr, src, dst, 1)
+        logpred_false = tree_logpredict(tree_root_ptr, src, dst, 0)
+        py_byte_str_1 = dataset_label_to_string(dataset_ptr, src)
+        py_byte_str_2 = dataset_label_to_string(dataset_ptr, dst)
+        py_boolean = value > 0
+        edges.append([py_byte_str_1.decode('ascii'),
+                      py_byte_str_2.decode('ascii'),
+                      py_boolean,
+                      logpred_true,
+                      logpred_false])
+    dataset_unref(dataset_ptr)
+    return edges
+
+cpdef bhcd(nx_obj, gamma=0.4, alpha=1.0, beta=0.2, delta=1.0, _lambda=0.2, binary_only=False, restarts=1, sparse=False, predict=False):
+    cdef GRand* rng_ptr
+    cdef Params* params_ptr
+    cdef Dataset* dataset_ptr = NULL
+    cdef Build* build_ptr
+    cdef Tree* tree_root_ptr
+    cdef int nedges, nvertices  
+    cdef gint next_index = -1
+    cdef Pair* cur
+    cdef Tree* tree_tmp_ptr
+    cdef gint parent_index
+    cdef GQueue * qq
+    cdef GList * child
+
+    construct_dataset_from_networkx_graph(nx_obj, dataset_ptr)
     rng_ptr = g_rand_new()
     params_ptr = params_new(dataset_ptr, <gdouble> gamma, <gdouble> alpha, 
         <gdouble> beta, <gdouble> delta, <gdouble> _lambda)    
@@ -159,7 +172,8 @@ cpdef bhcd(nx_obj, gamma=0.4, alpha=1.0, beta=0.2, delta=1.0, _lambda=0.2, binar
     # build json instance
     json_root = {"fit":{}}
     json_root["fit"]["logprob"] = tree_get_logprob(tree_root_ptr)
-    json_root["fit"]["edge"] = get_edge_prediction(dataset_ptr, tree_root_ptr)
+    if predict:
+        json_root["fit"]["edge"] = get_edge_prediction(len(nx_obj.edges), tree_root_ptr)
     
     tree_list = []
     qq = g_queue_new()
@@ -203,5 +217,6 @@ cpdef bhcd(nx_obj, gamma=0.4, alpha=1.0, beta=0.2, delta=1.0, _lambda=0.2, binar
     tree_ref(tree_root_ptr)
     build_free(build_ptr)
     tree_unref(tree_root_ptr)
+    dataset_unref(dataset_ptr)
     g_rand_free(rng_ptr)
     return json_root
